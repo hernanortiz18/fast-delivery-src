ceebf2f34cabab168941c564ddcc42b7c3434a86
fsosa44 <sosafrancisco2005@gmail.com>
2024-01-21 15:40:19 -0300

    feat: box template added, react-icons used

---
+++ fast-delivery-src/src/components/GetPackages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import boxPage from "../app/box/page"
+
+function GetPackages() {
+  return (
+    <div>
+        <boxPage/>
+    </div>
+  )
+}
+
+export default GetPackages
