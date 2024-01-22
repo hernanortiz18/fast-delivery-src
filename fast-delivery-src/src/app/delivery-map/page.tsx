d0bf2d822fc789d0e27c30553e8107653b17c707
FrancoPrandi <francoprandi55@gmail.com>
2024-01-22 12:27:10 -0300

    feat: register/box/mapDelivery updates

---
+++ fast-delivery-src/src/app/delivery-map/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/commons/Navbar";
+import DeliveryMapBox from "@/components/DeliveryMapBox";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Navbar />
+      <DeliveryMapBox />
+    </div>
+  );
+}
+
+export default page;
