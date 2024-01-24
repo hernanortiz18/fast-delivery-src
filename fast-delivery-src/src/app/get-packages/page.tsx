e34c2b0ec77278f684b4ddff1b44a494ce49fa64
FrancoPrandi <francoprandi55@gmail.com>
2024-01-24 16:30:55 -0300

    feat: maquetado updates

---
+++ fast-delivery-src/src/app/get-packages/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "@/commons/Navbar";
+import GetpackageBox from "@/components/GetPackagesBox";
+
+function Box() {
+  return (
+    <div>
+      <Navbar />
+      <GetpackageBox />
+    </div>
+  );
+}
+
+export default Box;
