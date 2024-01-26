d81f77b979b61efb2cc38fea4e60da7de44f4541
FrancoPrandi <francoprandi55@gmail.com>
2024-01-26 09:39:25 -0300

    feat: maquetado updates

--- fast-delivery-src/src/components/DeliveryMapBox.tsx
+++ fast-delivery-src/src/components/DeliveryMapBox.tsx
@@ -1,30 +1,52 @@
+"use client"
 import React from "react";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import "../styles/box.css";
 import "../styles/input.css";
 import "../styles/buttons.css";
+import { useRouter } from "next/navigation";
 
 function DeliveryMapBox() {
+  const router = useRouter();
+
+  const handleBackButton = () => {
+    router.back();
+  };
   return (
     <div
       style={{ display: "flex", flexDirection: "column", marginTop: "3.5rem" }}
     >
       <div className="headerBox">
-        <IoArrowBackCircleOutline className="arrowIcon" />
+          <IoArrowBackCircleOutline className="arrowIcon" onClick={handleBackButton}/>
         <div style={{ display: "flex", margin: "auto" }}>
           <h1 className="title">Reparto en curso</h1>
         </div>
       </div>
       <div className="boxDeliveryMapStyle">
-        <div className="mapContainer">
-    
+        <div className="mapContainer"></div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginBottom: "20px",
+          }}
+        >
+          <div className="deliveryMapInfo">
+            <p style={{ fontWeight: "bold" }}>Destino: ​ </p>
+            <p> Amenabar 2100, CABA</p>
+          </div>
+          <div className="deliveryMapInfo">
+            <p style={{ fontWeight: "bold" }}>Número de paquete: ​ </p>
+            <p> #0A235</p>
+          </div>
+          <div className="deliveryMapInfo">
+            <p style={{ fontWeight: "bold" }}>Recibe: ​ </p>
+            <p> Mauro Bruno</p>
+          </div>
         </div>
-        <div style={{display: "flex", flexDirection: "column", marginBottom: "20px" }}>
-          <div className="deliveryMapInfo"><p style={{fontWeight: "bold"}}>Destino: ​ </p><p> Amenabar 2100, CABA</p></div>
-          <div className="deliveryMapInfo"><p style={{fontWeight: "bold"}}>Número de paquete: ​ </p><p> #0A235</p></div>
-          <div className="deliveryMapInfo"><p style={{fontWeight: "bold"}}>Recibe: ​ </p><p> Mauro Bruno</p></div>
-        </div>
-        <button className="greenButton" style={{marginTop: "30px"}}>Finalizar</button>
+        <button className="greenButton" style={{ marginTop: "30px" }}>
+          Finalizar
+        </button>
       </div>
       <div style={{ display: "flex", margin: "15px auto" }}>
         <button className="transparentButton1">Cancelar entrega</button>
