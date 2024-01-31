0fc1129b0f4b3390c18762087e21ec114df5fc20
McHeph <juanarismendidiaz@gmail.com>
2024-01-31 17:08:49 -0300

    create packages admin page

---
+++ fast-delivery-src/src/app/packages-admin/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/commons/Navbar";
+import PackagesAdmin from "@/components/PackagesAdmin";
+import React from "react";
+
+function PackagesAdminPage() {
+    return (
+        <div>
+            <Navbar />
+            <PackagesAdmin />
+        </div>
+    );
+}
+
+export default PackagesAdminPage;
---
+++ fast-delivery-src/src/assets/SeeMoreArrow.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SeeMoreArrow() {
+  return (
+    <div>
+      <svg width="24" height="12" viewBox="0 0 24 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 2L12 10L22 2" stroke="#3D1DF3" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+
+    </div>
+  );
+}
+
+export default SeeMoreArrow;
