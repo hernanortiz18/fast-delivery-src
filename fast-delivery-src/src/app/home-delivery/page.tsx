f83464c4c49639ba3efbe6359d83aabc56b807b7
McHeph <juanarismendidiaz@gmail.com>
2024-01-31 17:40:30 -0300

    Home Delivery fix

--- fast-delivery-src/src/app/home-delivery/page.tsx
+++ fast-delivery-src/src/app/home-delivery/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import "../../styles/init.css";
+import "../../styles/homeDelivery.css";
 import "../../styles/input.css";
 import "../../styles/buttons.css";
 import Navbar from "@/commons/Navbar";
